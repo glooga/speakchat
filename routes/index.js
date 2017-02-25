14d4be01ed622a91d1072d4a1269ad952797900a
glooga <glooga@live.com>
2017-02-25 14:52:35 -0600

    added socket.io functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(io) {
+	var express = require('express');
+	var router = express.Router();
+	var count = 0;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+	io.on('connection', function(socket){
+		socket.on('chat-message', function(msg){
+			io.emit('chat-message', msg);
+		});
+		socket.on('connect', function(msg){
+			count++;
+			console.log("A user has connected");
+			io.emit('user-connected', "A user has connected");
+		});
+		socket.on('disconnect', function(msg){
+			count--;
+			console.log("A user has disconnected");
+			io.emit('user-disconnected', "A user has disconnected");
+		});
+	});
 
-module.exports = router;
+	/* GET home page. */
+	router.get('/', function(req, res, next) {
+	  res.render('index', { title: 'SpeakChat', count: count });
+	});
+
+	return router;
+};
