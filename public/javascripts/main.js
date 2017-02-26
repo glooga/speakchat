620bc2087af7af696cf8c6bf46503d2215c1e08c
glooga <glooga@live.com>
2017-02-25 18:42:26 -0600

    generates wav files

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,17 +3,17 @@ var socket = io.connect('http://localhost:3000');
 console.log("*hacker voice* im in");
 
 socket.on('connect', function() {
-	console.log("You've Connected!!!");
-    socket.emit('connect', "A user has connected.");
+    socket.emit('user-connected', "A user has connected.");
 });
 socket.on('chat-message', function(data) {
-
+	var audio = new Audio(data.msg);
+	audio.play();
 });
 socket.on('user-connected', function(data) {
-	var msg = new SpeechSynthesisUtterance(data);
-	window.speechSynthesis.speak(msg);
+	var audio = new Audio(data);
+	audio.play();
 });
 socket.on('user-disconnected', function(data) {
-	var msg = new SpeechSynthesisUtterance(data);
-	window.speechSynthesis.speak(msg);
+	var audio = new Audio(data);
+	audio.play();
 });
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
+var fs = require('fs');
+
+var text_to_speech = new TextToSpeechV1({
+    "url": "https://stream.watsonplatform.net/text-to-speech/api",
+    "username": "2197e94c-7b36-4fe9-9201-163ae829b54d",
+    "password": "GOaQck84Si3B"
+});
+
+
 module.exports = function(io) {
-	var express = require('express');
-	var router = express.Router();
-	var count = 0;
+    var count = 0;
 
-	io.on('connection', function(socket){
-		socket.on('chat-message', function(msg){
-			io.emit('chat-message', msg);
-		});
-		socket.on('connect', function(msg){
-			count++;
-			console.log("A user has connected");
-			io.emit('user-connected', "A user has connected");
-		});
-		socket.on('disconnect', function(msg){
-			count--;
-			console.log("A user has disconnected");
-			io.emit('user-disconnected', "A user has disconnected");
-		});
-	});
+    io.on('connection', function(socket) {
+        socket.on('chat-message', function(data) {
+            var name = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10);
+            var params = {
+                text: data.msg,
+                voice: 'en-US_AllisonVoice', // Optional voice
+                accept: 'audio/wav'
+            };
 
-	/* GET home page. */
-	router.get('/', function(req, res, next) {
-	  res.render('index', { title: 'SpeakChat', count: count });
-	});
+            // Pipe the synthesized text to a file
+            text_to_speech.synthesize(params).pipe(fs.createWriteStream('output.wav'));
+            io.emit('chat-message', { user: data.user, msg: "/memes/" + name + ".wav" });
+        });
+        socket.on('user-connected', function(msg) {
+            count++;
+			io.emit('user-connected', { user: data.user, msg: "/memes/connected.wav" });
+        });
+        socket.on('disconnect', function(msg) {
+            count--;
+			io.emit('user-connected', { user: data.user, msg: "/memes/disconnected.wav" });
+        });
+    });
 
-	return router;
+    /* GET home page. */
+    router.get('/', function(req, res, next) {
+        res.render('index', {
+            title: 'SpeakChat',
+            count: count
+        });
+    });
+
+    return router;
 };
